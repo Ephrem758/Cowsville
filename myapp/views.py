41084cb524d4c671ffda825791957d6a02b9d16b
Samuel Girma Megra <samigirma53@gmail.com>
2024-10-06 21:56:42 +0300

    models

--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Farm, Animal
 
 # Create your views here.
 
@@ -33,3 +35,30 @@ def sign_in(request):
 
 def sign_up(request):
     return render(request, 'sign-up.html')
+
+
+def farm_details(request):
+    farms = Farm.objects.all()  # Get all farms for the dropdown
+    farm = None
+    cows = None
+    cow_details = None
+    cow_id = None
+
+    if request.method == 'GET':
+        farm_id = request.GET.get('farm_id', None)
+        cow_id = request.GET.get('cow_id', None)
+        
+        if farm_id:
+            farm = get_object_or_404(Farm, id=farm_id)
+            cows = Animal.objects.filter(farm=farm)
+        
+        if cow_id:
+            cow_details = get_object_or_404(Animal, cow_id=cow_id)
+
+    return render(request, 'farm_details.html', {
+        'farms': farms,
+        'farm': farm,
+        'cows': cows,
+        'cow_details': cow_details,
+        'cow_id': cow_id
+    })
