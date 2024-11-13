b99a4898a85d01a82b9095f5dfa9d44c3b7da84a
Samuel Girma Megra <samigirma53@gmail.com>
2024-11-13 11:21:24 +0300

    alert system added

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
 from django.urls import path
+from .views import ReportHeatSignView
 
 
 from . import views
@@ -19,9 +20,11 @@ urlpatterns = [
     # path('profile/', views.profile, name='profile'),
     path('sign-in/', views.sign_in, name='sign_in'),
     path('sign-up/', views.sign_up, name='sign_up'),
-    path('dashboard/search/', views.search_farm, name='search_farm'),
-    path('dashboard/search/animal/', views.search_animal, name='search_animal'),
     # path('farm-details/', views.farm_details, name='farm_details'),
+    path('api/report-heat-sign/<str:cow_id>/', ReportHeatSignView.as_view(), name='report-heat-sign'),
+    path('search_farm/', views.search_farm, name='search_farm'),
+    path('search_animal/', views.search_animal, name='search_animal'),
+
     # path('pages/sign-in/', views.sign_in, name='sign_in'),
     # path('pages/dashboard/', views.dashboard, name='dashboard'),
     # path('pages/tables/', views.tables, name='tables'),
