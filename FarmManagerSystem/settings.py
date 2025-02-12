0af8d4ae2dafb5abf55d6c225346ae0f9fe32d67
Ephrem Alemayehu <ephremalemayehu2019@gmail.com>
2025-02-12 18:11:23 +0300

    procfile

--- FarmManagerSystem/settings.py
+++ FarmManagerSystem/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^nvczssod(!rcglw2)dhy)looo$0$ssiydr4_ok&1_82esqt7+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'enlargement-tariff-tennis-reconstruction.trycloudflare.com']
 
 
 # Application definition
